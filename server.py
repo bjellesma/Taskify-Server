a842c581e99b35d70aba707f8958ca02ec2778fe
bjellesma <william.jellesma@gmail.com>
2019-07-06 23:44:09 -0400

    route to get all tasks of list (hardcoded)

--- server.py
+++ server.py
@@ -1,6 +1,39 @@
-from flask import Flask
+from flask import Flask, request
+import json
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def get_tasks(task_param):
+    tasks = []
+    task_list = [
+        {
+            'id': 1,
+            'list_name': 'critical',
+            'task_display': 'Feed the dogs'
+        },
+        {
+            'id': 2,
+            'list_name': 'critical',
+            'task_display': 'Do not feed the goblin after midnight'
+        },
+        {
+            'id': 3,
+            'list_name': 'high',
+            'task_display': 'Have fun'
+        }
+        
+    ]
+    for task in task_list:
+        if task['list_name'] == task_param:
+            tasks.append(task)
+    return tasks
+
+@app.route("/api/getlisttasks", methods=["GET"])
+def get_list_tasks():
+    list_name = request.args.get('list')
+    # TODO get tasks with the list name from mongo
+    # For now, we are hardcoding the values
+    tasks = get_tasks(list_name)
+    return json.dumps(tasks)
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 3001)
---
+++ taskify-server_env.sh
@@ -0,0 +1,3 @@
+# simple shell script to activate the virtual environment for this server
+# source taskify-server_env.sh
+source taskify-server/bin/activate
